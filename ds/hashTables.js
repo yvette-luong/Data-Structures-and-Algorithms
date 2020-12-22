2d16a49d1f853ddada9570deaf3749687c5667b5
yvette-luong <63530290+yvette-luong@users.noreply.github.com>
2020-12-22 13:18:53 -0800

    bonus keys() without collision

--- ds/hashTables.js
+++ ds/hashTables.js
@@ -10,14 +10,14 @@ class HashTable {
     let hash = 0;
     for (let i = 0; i < key.length; i++) {
       hash = (hash + key.charCodeAt(i) * i) % this.data.length;
-    //   console.log(hash)
-    } 
+      //   console.log(hash)
+    }
     return hash;
   } //O(1)
 
   set(key, value) {
     let address = this._hash(key); //store data that we gonna insert with the key and value in this address place that we created by our hashed function, and we give it the key of "grapes"
-    if (!this.data[address]) { 
+    if (!this.data[address]) {
       this.data[address] = [];
     }
     this.data[address].push([key, value]);
@@ -29,14 +29,48 @@ class HashTable {
     const address = this._hash(key);
     const currentBucket = this.data[address];
     if (currentBucket) {
-      for (let i = 0; i < currentBucket.length; i++) { 
-        if (currentBucket[i][0] === key) { // if current bucket by which is the index so remember is going to say zero which is going to grab this then one which is going to grab this if 'currentBucket' the grapes. So if I do zero here that means grab the first in our case is the [i] so its gonna be the 0 first grab this first array then grab the zero index within this array which will be grapes.
+      for (let i = 0; i < currentBucket.length; i++) {
+        if (currentBucket[i][0] === key) {
+          // if current bucket by which is the index so remember is going to say zero which is going to grab this then one which is going to grab this if 'currentBucket' the grapes. So if I do zero here that means grab the first in our case is the [i] so its gonna be the 0 first grab this first array then grab the zero index within this array which will be grapes.
           return currentBucket[i][1];
         }
       }
-    } //O(1) 
+    } //O(1)
     return undefined;
-  } 
+  }
+
+  keys() {
+    const keyArrays = [];
+    // console.log(this.data.length);
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i]) {
+        keyArrays.push(this.data[i][0][0]);
+      }
+    }
+    return keyArrays;
+  }
+  //bonus : key()without collision
+  // key() {
+  //   if(!this.data.length) {
+  //     return undefined
+  //   }
+  //   let result = []
+  //   //loop through all the elements
+  //   for (let i = 0 ; i < this.data.length; i++) {
+  //     //if it's not an empty memory cell
+  //     if (this.data[i] && this.data[i].length) {
+  //       //but also loop althrough the potential collisions
+  //       if (this.data.length > 1) {
+  //         for ( let j = 0; j < this.data[i].length; j++) {
+  //           result.push(this.data)[i][j][0]
+  //         }
+  //       } else {
+  //         result.push(this.data[i][0])
+  //       }
+  //     }
+  //   }
+  //   return result;
+  // }
 }
 
 const myHashTable = new HashTable(50);
@@ -44,3 +78,4 @@ console.log(myHashTable.set("grapes", 10000));
 myHashTable.get("grapes");
 myHashTable.set("apples", 9);
 myHashTable.get("apples");
+myHashTable.keys();
